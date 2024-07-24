3548513c5001efea335e93f5fddf929de9ca300e
Na young <70385710+nayeo2@users.noreply.github.com>
2024-07-24 14:37:59 +0900

    Update update_crontab.sh

--- update_crontab.sh
+++ update_crontab.sh
@@ -1,17 +1,19 @@
-
 #!/bin/bash
 
 # 크론탭에서 제거할 구문
-CRON_JOB="*/5 * * * * sudo /home/ubuntu/resource_usage.sh"
+OLD_CRON_JOB="*/5 * * * * sudo /home/ubuntu/resource_usage.sh"
+
+# 새 크론잡 구문 (1분마다 실행)
+NEW_CRON_JOB="* * * * * sudo /home/ubuntu/resource_usage.sh"
 
 # 현재 사용자 크론탭을 임시 파일에 백업
 crontab -l > current_crontab.txt 2>/dev/null
 
 # 임시 파일에서 특정 구문을 포함하지 않는 라인만을 새로운 파일로 저장
-grep -v "/home/ubuntu/resource_usage.sh" current_crontab.txt > new_crontab.txt
+grep -v "$OLD_CRON_JOB" current_crontab.txt > new_crontab.txt
 
-# 새로운 크론탭 파일에 항상 구문을 추가
-echo "$CRON_JOB" >> new_crontab.txt
+# 새로운 크론탭 파일에 새 구문을 추가
+echo "$NEW_CRON_JOB" >> new_crontab.txt
 
 # 새로운 크론탭 파일을 적용
 crontab new_crontab.txt
@@ -19,4 +21,4 @@ crontab new_crontab.txt
 # 임시 파일 삭제
 rm current_crontab.txt new_crontab.txt
 
-echo "크론탭에서 '$CRON_JOB' 구문을 갱신했습니다."
+echo "크론탭에서 '$OLD_CRON_JOB' 구문을 제거하고 '$NEW_CRON_JOB' 구문을 추가했습니다."
